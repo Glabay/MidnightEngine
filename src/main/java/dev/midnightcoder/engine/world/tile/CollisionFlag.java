904f44b28e59b7f763682f9e4d81a521feae9e99
Glabay <Java_Guru@live.com>
2026-05-01 02:34:17 -0300

    feat: Colission flags
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/engine/world/tile/CollisionFlag.java
@@ -0,0 +1,41 @@
+package dev.midnightcoder.engine.world.tile;
+
+import java.util.Arrays;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-04-30
+ */
+public enum CollisionFlag {
+    NONE(0),
+
+    NORTH(1 << 0),
+    SOUTH(1 << 1),
+    EAST (1 << 2),
+    WEST (1 << 3),
+
+    FULL(NORTH.mask | SOUTH.mask | EAST.mask | WEST.mask)
+    ;
+
+    private final int mask;
+
+    CollisionFlag(int mask) {
+        this.mask = mask;
+    }
+
+    public int getMask() {
+        return mask;
+    }
+
+    public static boolean has(int flags, CollisionFlag flag) {
+        return (flags & flag.mask) != 0;
+    }
+
+    public static int combine(CollisionFlag... flags) {
+        return Arrays.stream(flags)
+            .mapToInt(f -> f.mask)
+            .reduce(0, (a, b) -> a | b);
+    }
+}
