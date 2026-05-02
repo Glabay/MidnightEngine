9de5e56949b49e8650d0ef85d4f8492181c23dea
Glabay <Java_Guru@live.com>
2026-05-02 13:44:39 -0300

    chore: Movement system improvements to prepare for NPC Movement
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/engine/system/Movement.java
+++ src/main/java/dev/midnightcoder/engine/system/Movement.java
@@ -4,82 +4,22 @@ import dev.midnightcoder.engine.entity.Entity;
 import dev.midnightcoder.engine.world.TileMap;
 import dev.midnightcoder.engine.world.tile.CollisionFlag;
 
-import static dev.midnightcoder.engine.world.tile.Tile.TILE_SIZE;
-
 /**
  * @author Glabay | Glabay-Studios
  * @project MidnightRPG
  * @social Discord: Glabay
- * @since 2026-04-30
+ * @since 2026-05-02
  */
-public class Movement {
+public abstract class Movement {
     private final TileMap tileMap;
 
     public Movement(TileMap tileMap) {
         this.tileMap = tileMap;
     }
 
-    public void move(Entity entity, int dx, int dy) {
-        if (dx != 0) {
-            entity.setX(entity.getX() + dx);
-            resolveX(entity, dx);
-        }
-        if (dy != 0) {
-            entity.setY(entity.getY() + dy);
-            resolveY(entity, dy);
-        }
-    }
+    public abstract void move(Entity mob, int dx, int dy);
 
-    private void resolveX(Entity entity, int dx) {
-        int top = entity.getY() / TILE_SIZE;
-        int bottom =(entity.getY() + entity.getHeight() - 1) / TILE_SIZE;
-
-        if (dx > 0) {
-            var right = (entity.getX() + entity.getWidth() - 1) / TILE_SIZE;
-
-            for (int ty = top; ty <= bottom; ty++) {
-                // Look left
-                if (isBlocked(right, ty)) {
-                    entity.setX(right * TILE_SIZE - entity.getWidth());
-                    break;
-                }
-            }
-        }
-        else if (dx < 0) {
-            var left = entity.getX() / TILE_SIZE;
-
-            for (int ty = top; ty <= bottom; ty++) {
-                // Look left
-                if (isBlocked(left, ty)) {
-                    entity.setX((left + 1) * TILE_SIZE);
-                    break;
-                }
-            }
-        }
-    }
-
-    private void resolveY(Entity entity, int dy) {
-        var left = entity.getX() / TILE_SIZE;
-        var right = (entity.getX() + entity.getWidth() -1) / TILE_SIZE;
-
-        var top = entity.getY() / TILE_SIZE;
-        var bottom = (entity.getY() + entity.getHeight() -1) / TILE_SIZE;
-
-        for (int tx = left; tx <= right; tx++) {
-            // look down
-            if (dy > 0 && isBlocked(tx, bottom)) {
-                entity.setY(top * TILE_SIZE);
-                break;
-            }
-            // look up
-            if (dy < 0 && isBlocked(tx, top)) {
-                entity.setY(bottom * TILE_SIZE);
-                break;
-            }
-        }
-    }
-
-    private boolean isBlocked(int tileX, int tileY) {
+    protected boolean isBlocked(int tileX, int tileY) {
         var tile = tileMap.getTile(tileX, tileY);
         return tile != null &&
             tile.type().getCollisionFlags() != CollisionFlag.NONE.getMask();
