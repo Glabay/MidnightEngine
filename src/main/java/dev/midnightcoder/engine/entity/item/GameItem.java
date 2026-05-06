135d440035697ac721a108dcc1b3e57e584ae2f8
Glabay <Java_Guru@live.com>
2026-05-06 15:16:51 -0300

    Chore: Added an abstract GameItem class
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/engine/entity/item/GameItem.java
@@ -0,0 +1,13 @@
+package dev.midnightcoder.engine.entity.item;
+
+import dev.midnightcoder.engine.renderer.graphics.Texture;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-05-06
+ */
+public abstract class GameItem {
+    protected Texture icon;
+}
