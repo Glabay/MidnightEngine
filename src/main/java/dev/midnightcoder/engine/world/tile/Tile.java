2c4c8054cc8904b11d5c9a31ae6afae17e289e6a
Glabay <Java_Guru@live.com>
2026-05-06 10:26:48 -0300

    chore: removed unused color identifiers
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/engine/world/tile/Tile.java
+++ src/main/java/dev/midnightcoder/engine/world/tile/Tile.java
@@ -1,5 +1,7 @@
 package dev.midnightcoder.engine.world.tile;
 
+import java.awt.*;
+
 /**
  * @author Glabay | Glabay-Studios
  * @project MidnightRPG
@@ -13,19 +15,7 @@ public record Tile(
 ) {
     public static final int TILE_SIZE = 32;
 
-    /// Color matching
-    public static final int	COL_STONE_WALL		= 0xFF7F7F7F;
-    public static final int	COL_STONE_WALL_TOP	= 0xFF6F6F6F;
-    public static final int	COL_WOOD_WALL		= 0xFF774500;
-    public static final int	COL_WOOD_WALL_TOP	= 0xFF603800;
-
-    public static final int	COL_WOOD_FLOOR	    = 0xFF7F7F00;
-    public static final int	COL_STONE_FLOOR	    = 0xFF7B8C7E;
-    public static final int	COL_GRASS		    = 0xFF00FF00;
-    public static final int	COL_DIRT		    = 0;
-
-    public static final int	COL_GARDEN_PATCH	= 0xFF2A882A;
-    public static final int	COL_WATER_TILE		= 0xFF0000FF;
-    public static final int	COL_STONE_PATH		= 0xFFA0A0A0;
-
+    public Rectangle getHitbox(int tileX, int tileY) {
+        return type.getHitbox(tileX, tileY);
+    }
 }
